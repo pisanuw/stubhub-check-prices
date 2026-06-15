5f9411d2d2cd939e2b819685e2e596669031e685
Yusuf Pisan <pisan@uw.edu>
2026-06-14 22:02:59 -0700

    add supabase query

---
+++ supabase-query.sh
@@ -0,0 +1,6 @@
+npm run query latest              # cheapest per class right now
+npm run query daily Hospitality   # daily low/avg/max for one class
+npm run query runs 24             # how many checks ran in last 24h + last time
+npm run query watch               # are Champions Club / Trophy Lounge / FIFA Pavilion listed yet?
+npm run query raw "stubhub_price_snapshots?select=*&limit=5"   # any PostgREST query
+
